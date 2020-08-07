0fb0edece7948077ef030c1b8f0bbbcfd0b77273
Avi Peretz <pavi@cambium.co.il>
2020-08-07 12:44:50 +0300

    CocktailDetails screen - update styles

--- src/screens/CocktailDetails/CocktailDetails.js
+++ src/screens/CocktailDetails/CocktailDetails.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
-import { View, Text, ScrollView, Image, TouchableOpacity, Linking } from 'react-native';
-import { Card, List, ListItem, Left, Body, Right, Thumbnail, Header, Button, Title, Icon, Spinner } from 'native-base';
+import { View, Text, ScrollView, ImageBackground } from 'react-native';
+import { List, ListItem, Left, Body, Right, Thumbnail, Header, Button, Title, Icon, Spinner } from 'native-base';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCocktailById } from '../../store/actions/CocktailsActions';
 import styles from './style';
 import { IMAGES_URL } from '@env';
+import DarkenImg from '../../components/DarkenImg/DarkenImg';
 
 const CocktailDetails = props => {
 
@@ -17,7 +18,7 @@ const CocktailDetails = props => {
     useEffect(() => {
         dispatch(getCocktailById(id))
     }, [dispatch])
-    
+
     if (Object.keys(selectedCocktail).length > 0) {
         const ingredientAndMeasure = selectedCocktail.ingredientList.map((ingredient, index) => {
             return (
@@ -36,58 +37,47 @@ const CocktailDetails = props => {
         })
         return (
             <ScrollView>
-                <Header>
-                    <Left>
-                        <Button transparent onPress={() => navigation.goBack()}>
-                            <Icon name='arrow-back' />
-                        </Button>
-                    </Left>
-                    <Body>
-                        <Title>{name}</Title>
-                    </Body>
-                    <Right>
-                        <Button transparent onPress={() => { }}>
-                            <Icon name={'star-outline'} />
-                        </Button>
-                        <Button transparent onPress={() => navigation.navigate("Home")}>
-                            <Icon name='home' />
-                        </Button>
-                    </Right>
-                </Header>
                 <ScrollView>
-                    <Image
-                        style={styles.image}
-                        source={{ uri: selectedCocktail.strDrinkThumb }}
-                    />
-                    <View style={{ padding: 7, marginTop: 10 }}>
-                        <Text style={styles.title}>Ingredients</Text>
-                        <Card>
-                            <List>{ingredientAndMeasure}</List>
-                        </Card>
-                        <Text style={{ ...styles.title, marginTop: 15 }}>Instructions</Text>
-                        <Card>
-                            <View style={{ padding: 7 }}>
-                                <Text style={styles.content}>{selectedCocktail.strInstructions}</Text>
+                    <ImageBackground style={styles.image} source={{ uri: selectedCocktail.strDrinkThumb }}>
+                        <DarkenImg>
+                            <Header style={styles.header}>
+                                <Left>
+                                    <Button transparent onPress={() => navigation.goBack()}>
+                                        <Icon name='arrow-back' />
+                                    </Button>
+                                </Left>
+                                <Right>
+                                    <Button transparent onPress={() => { }}>
+                                        <Icon name={'star-outline'} />
+                                    </Button>
+                                    <Button transparent onPress={() => navigation.navigate("Home")}>
+                                        <Icon name='home' />
+                                    </Button>
+                                </Right>
+                            </Header>
+                            <View style={styles.titleContainer}>
+                                <Title style={styles.title}>{name}</Title>
                             </View>
-                            <TouchableOpacity
-                                style={styles.iconContainer}
-                                activeOpacity={0.6}
-                                onPress={() => Linking.openURL(selectedCocktail.strYoutube)} >
-                                <Icon
-                                    size={40}
-                                    name="logo-youtube"
-                                    color="#ff0000"
-                                />
-                                <Text style={styles.iconTitle}>Recipe Video</Text>
-                            </TouchableOpacity>
-                        </Card>
+                        </DarkenImg>
+                    </ImageBackground>
+                    <View style={styles.container}>
+                        <View header style={styles.subTitleContainer}>
+                            <Text style={styles.subTitle}>Ingredients</Text>
+                        </View>
+                        <List>{ingredientAndMeasure}</List>
+                        <View header style={styles.subTitleContainer}>
+                            <Text style={styles.subTitle}>Instructions</Text>
+                        </View>
+                        <View style={{ padding: 20, paddingTop: 10 }}>
+                            <Text style={styles.content}>{selectedCocktail.strInstructions}</Text>
+                        </View>
                     </View>
                 </ScrollView>
             </ScrollView>
         )
     } else {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <View style={styles.spinnerContainer}>
                 <Spinner color={'blue'} />
             </View>
         )
--- src/screens/CocktailDetails/style.js
+++ src/screens/CocktailDetails/style.js
@@ -3,16 +3,39 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     image: {
         width: "100%",
-        height: 200
+        height: 320,
+        position: 'relative'
+    },
+    header: {
+        backgroundColor: 'transparent',
+        elevation: 0
+    },
+    titleContainer: {
+        position: 'absolute',
+        bottom: 0,
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        paddingVertical: 10,
+        width: '100%'
+    },
+    title: {
+        fontSize: 20,
+    },
+    subTitleContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 20 
+    },
+    subTitle: {
+        fontWeight: 'bold',
+        fontSize: 17,
+    },
+    container: {
+        backgroundColor: 'white'
     },
     ingredientsContainer: {
         flexDirection: "row",
         alignItems: "center",
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: "bold",
-        textAlign: "center"
     },
     ingredientImage: {
         width: 50,
@@ -42,7 +65,12 @@ const styles = StyleSheet.create({
     },
     col: {
         width: '33%',
-    }    
+    },
+    spinnerContainer: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    }
 })
-  
+
 export default styles;
