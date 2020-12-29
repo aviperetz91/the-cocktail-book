982203f8a6d4b7692b56f1b9354fd1541ff128b1
Avi <a.peretz33@gmail.com>
2020-12-30 00:16:11 +0200

    Save review also to the user's table

--- src/store/actions/ReviewsActions.js
+++ src/store/actions/ReviewsActions.js
@@ -2,36 +2,53 @@ import database from '@react-native-firebase/database';
 
 export const LEAVE_FEEDBACK = 'LEAVE_FEEDBACK';
 export const GET_REVIEWS = 'GET_REVIEWS';
+export const SET_REVIEWS_ERROR = 'SET_REVIEWS_ERROR';
 
 
 export const leaveFeedback = (idDrink, token, userId, userName, rating, content) => {
     return async dispatch => {
         const dateNow = Date.now()
-        await database().ref(`/reviews/${idDrink}/${userId}_${dateNow}`).set({
+        const review = {
             userId: userId,
             autor: userName,
             rating: rating,
             content: content,
             date: new Date(dateNow)
-        })
+        }
+        try {
+            await database().ref(`/reviews/${idDrink}/${dateNow}`).set(review)
+            await database().ref(`/users/${userId}/reviews/${dateNow}`).set(review)
+        } catch (err) {
+            let error;
+            if (err.code === 'database/permission-denied') {
+                error = 'You need to be logged in to do that';
+            } else {
+                error = error.message;
+            }
+            dispatch({ type: SET_REVIEWS_ERROR, error: error })
+        }
     }
 }
 
 export const getReviewsById = (idDrink) => {
     return async dispatch => {
-        const snapshot = await database().ref(`/reviews/${idDrink}`).once('value');
-        const revObj = snapshot.val();
-        const reviews = [];
-        for (let i in revObj) {
-            reviews.push(revObj[i])
-        }
-        let ratingSum = 0, ratingAvg = 0;
-        if (reviews.length > 0) {
-            reviews.forEach(rev => ratingSum += rev.rating);
-            ratingAvg = ratingSum / reviews.length;
-            dispatch({ type: GET_REVIEWS, reviews, ratingAvg })
-        } else {
-            dispatch({ type: GET_REVIEWS, reviews })
+        try {
+            const snapshot = await database().ref(`/reviews/${idDrink}`).once('value');
+            const revObj = snapshot.val();
+            const reviews = [];
+            for (let i in revObj) {
+                reviews.push(revObj[i])
+            }
+            let ratingSum = 0, ratingAvg = 0;
+            if (reviews.length > 0) {
+                reviews.forEach(rev => ratingSum += rev.rating);
+                ratingAvg = ratingSum / reviews.length;
+                dispatch({ type: GET_REVIEWS, reviews, ratingAvg })
+            } else {
+                dispatch({ type: GET_REVIEWS, reviews })
+            }
+        } catch (err) {
+            dispatch({ type: SET_REVIEWS_ERROR, error: err.message })
         }
     }
 }
--- src/store/reducers/ReviewsReducer.js
+++ src/store/reducers/ReviewsReducer.js
@@ -1,16 +1,23 @@
-import { GET_REVIEWS } from '../actions/ReviewsActions';
+import { GET_REVIEWS, SET_REVIEWS_ERROR } from '../actions/ReviewsActions';
 
 const initialState = {
     reviews: null,
-    ratingAvg: 0
+    ratingAvg: 0,
+    reviewsError: null
 }
 
 const ReviewsReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_REVIEWS:
             return {
+                ...state,
                 reviews: action.reviews,
                 ratingAvg: action.ratingAvg ? action.ratingAvg : 0
+            }
+        case SET_REVIEWS_ERROR:
+            return {
+                ...state,
+                reviewsError: action.error
             }
         default:
             return state;
