ade2b87e86bf3b5b4d2598b72ffda77c68527f16
Avi <a.peretz33@gmail.com>
2021-04-04 22:05:48 +0300

    SearchBar update

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,66 +1,35 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { View, TouchableOpacity, Platform } from 'react-native';
-import { Header, Item, Input, Icon, Thumbnail } from 'native-base';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { View } from 'react-native';
+import { Header, Item, Input, Icon, } from 'native-base';
 import CocktailList from '../CocktailList/CocktailList';
-import { SearchBar } from 'react-native-elements';
-import { getCocktailByName, clearData } from '../../store/actions/CocktailsActions';
 import styles from './style';
 
 const Search = props => {
 
     const { navigation, closeSearch, searchInput, setSearchInput } = props;
-    const { searchResults } = useSelector(state => state.cocktails)
-    // const searchBarRef = React.createRef();
-
-    const dispatch = useDispatch();
-
-    const searchHandler = input => {
-        if (input === '') {
-            dispatch(clearData('searchResults'))
-        } else {
-            dispatch(getCocktailByName(input))
-        }
-    }
+    const { cocktails } = useSelector(state => state.cocktails)
 
     const handleClose = () => {
         setSearchInput('');
-        dispatch(clearData('searchResults'));
         closeSearch()
-    }
-
-    const changeTextHandler = (input) => {
-        searchHandler(input);
-        setSearchInput(input);
     }
 
     return (
         <View style={styles.screen}>
-            {/* {Platform.OS === 'ios' && <Header style={styles.header} />}
-            {Platform.OS === 'android' && <View style={styles.m_t} ></View> }
-            <SearchBar
-                ref={searchBarRef}
-                placeholder="Search Cocktail..."
-                onChangeText={(input) => changeTextHandler(input)}
-                value={searchInput}
-                autoFocus
-                searchIcon={
-                    <TouchableOpacity onPress={goBack}>
-                        <Icon name='arrow-back-outline' style={styles.arrowBack} />
-                    </TouchableOpacity>
-                }
-                lightTheme
-                containerStyle={styles.searchBar}
-                inputContainerStyle={styles.input}
-            /> */}
             <Header searchBar rounded androidStatusBarColor={'rgba(0,0,0,0.4)'} iosBarStyle={'light-content'} translucent style={styles.header}>
                 <Item>
                     <Icon type="MaterialCommunityIcons" name="keyboard-backspace" onPress={handleClose} />
-                    <Input placeholder="Search Cocktail" onChangeText={(input) => changeTextHandler(input)} value={searchInput} autoFocus />
-                    {searchInput ? <Icon name="close-outline" onPress={(input) => changeTextHandler(input)} /> : null}
+                    <Input placeholder="Search Cocktail" onChangeText={(input) => setSearchInput(input)} value={searchInput} autoFocus />
+                    {searchInput ? <Icon name="close-outline" onPress={(input) => setSearchInput(input)} /> : null}
                 </Item>
             </Header>
-            {searchInput !== '' && <CocktailList navigation={navigation} cocktails={searchResults} /> }
+            {searchInput !== '' && 
+                <CocktailList 
+                    navigation={navigation} 
+                    cocktails={cocktails.filter(drink => drink.strDrink.toLowerCase().includes(searchInput.toLowerCase()))} 
+                /> 
+            }
         </View >
     );
 }
