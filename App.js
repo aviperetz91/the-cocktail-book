998768226a020ea65366044809292bc4edc95709
Avi <a.peretz33@gmail.com>
2021-04-21 15:21:13 +0300

    Implement auto logout

--- App.js
+++ App.js
@@ -1,35 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
-import { setUserDetails } from './src/store/actions/UserActions';
+import { setUserDetails, signout } from './src/store/actions/UserActions';
 import auth from '@react-native-firebase/auth';
 import Auth from './src/screens/Auth/Auth';
 import StackNavigator from './src/navigation/StackNavigator';
 import Spinner from './src/components/Spinner/Spinner';
 
-
 const App = () => {
 
-  const [user, setUser] = useState();
+  const [userData, setUserData] = useState();
   const dispatch = useDispatch();
 
-  const onAuthStateChanged = user => {
-    if (user) {
-      dispatch(setUserDetails(user.uid))
+  let userSessionTimeout = null;
+
+  const authStateChanged = async (user) => {
+    if (user === null && userSessionTimeout) {
+      clearTimeout(userSessionTimeout);
+      userSessionTimeout = null;
+    } else {
+      try {
+        const idTokenResult = await auth().currentUser.getIdTokenResult();
+        const authTime = idTokenResult.claims.auth_time * 1000;
+        const sessionDurationInMilliseconds = 60 * 60 * 1000;
+        const expirationInMilliseconds = sessionDurationInMilliseconds - (Date.now() - authTime);
+        userSessionTimeout = setTimeout(() => dispatch(signout()), expirationInMilliseconds);
+        dispatch(setUserDetails(user.uid))
+      } catch(err) {
+        console.log(err)
+      }
     }
-    setUser(user);
+    setUserData(user)
   }
 
   useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    const subscriber = auth().onAuthStateChanged(authStateChanged);
     return () => subscriber;
   }, []);
 
-  if (user === undefined) {
+  if (userData === undefined) {
     return (
       <Spinner />
     )
-  } else if (user) {
+  } else if (userData) {
     return (
       <NavigationContainer>
         <StackNavigator />
